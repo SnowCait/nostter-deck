983a15ac13fb009fd4fba2f2c84ddbe29f369abc
雪猫 <SnowCait@users.noreply.github.com>
2026-05-22 02:20:20 +0000

    Lang

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { getLocale } from '$lib/paraglide/runtime.js';
 	import './layout.css';
 
 	const { children } = $props();
+
+	onMount(() => {
+		document.documentElement.lang = getLocale();
+	});
 </script>
 
 {@render children()}
